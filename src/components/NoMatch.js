3fd78e172e775ec0b9c0eaf379e8cca5d84c7e56
Varshn29 <varshininarayana29@gmail.com>
2022-09-14 22:10:34 +0530

    modified NoMatch

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const NoMatch = () => {
-  return (
-    <div>NoMatch Found</div>
-  )
+    return (
+        <div style={{ textAlign: 'center', fontSize: '70px', marginTop: '150px' }}>
+            NoMatch Found!!!!!!!!!
+        </div>
+    )
 }
 
 export default NoMatch;
