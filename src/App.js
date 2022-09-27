59ef4c0ed85bb2434e12b284b182c6272c2c0114
Varshn29 <varshininarayana29@gmail.com>
2022-09-27 20:33:16 +0530

    added letter count

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState('')
+
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const handleClick = (value) => {
+
+    let count = {};
+
+    for (let i = 0; i < value.length; i++) {
+      if (count[value[i]]) {
+        count[value[i]]++;
+      } else {
+        count[value[i]] = 1
+      }
+    }
+    for (let key in count) {
+      if (count[key] > 1)
+        console.log(key + ", count = " + count[key]);
+    }
+
+  }
+
+
+
   return (
-    <div> test </div>
+    <div>
+      <input type={'text'} value={value} onChange={handleChange} />
+      <button onClick={() => handleClick(value)}>Submit</button>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import Router from './router/appRouter';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
